a88ac83991b7fd610b207876ae8dae816c00ea33
Sven Seeberg <mail@sven-seeberg.de>
2026-04-24 09:33:36 +0200

    Improve error handling & logging

--- src/salt_passbolt/__init__.py
+++ src/salt_passbolt/__init__.py
@@ -1,17 +1,25 @@
 """
 Provides functions to fetch passwords from passbolt api
 """
+import json
+import logging
+import uuid
+
 import passboltapi  # pylint: disable=E0401
 
+logger = logging.getLogger(__name__)
 
-def get_password_list(passbolt_obj, group_uuid):
+def get_password_list(passbolt_obj: passboltapi.PassboltAPI, group_uuid: str) -> list[dict]:
     """
     Get list of passwords for group UUID
     """
     result = list()
     url = ("/resources.json?filter[is-shared-with-group]" +
            "={}&api-version=v2".format(group_uuid))
-    for i in passbolt_obj.get(url)["body"]:  # pylint: disable=C0301
+    passwords = passbolt_obj.get(url)["body"]
+    if not passwords:
+        raise ValueError("Got empty password list from Passbolt server.")
+    for i in passwords:
         result.append({
             "id": i["id"],
             "name": i["name"],
@@ -21,38 +29,65 @@ def get_password_list(passbolt_obj, group_uuid):
     return result
 
 
-def generate_pillar(passbolt_obj, group_uuid):
+def generate_pillar(passbolt_obj: passboltapi.PassboltAPI, group_uuid: str) -> dict:
     """
     Generate dictionary that is added to the Pillar
     """
-    result = get_password_list(passbolt_obj, group_uuid)
+    try:
+        result = get_password_list(passbolt_obj, group_uuid)
+    except Exception as e:
+        logger.error(f"Failed to obtain Passbolt object list for group {group_uuid}. Verify that the Salt Master Passbolt user has access to this group.")
+        logger.exception(e)
+        return {}
     salt = {'passbolt': {}}
     for i in result:
-        resource = passbolt_obj.get("/secrets/resource/{}.json?api-version=v2".
-                                    format(i["id"]))
-        data = decode_json(passbolt_obj.decrypt(resource["body"]["data"]))
-        assert data, "Passbolt returned an empty secret."
-        salt['passbolt'][i["id"]] = data
+        try:
+            resource = passbolt_obj.get("/secrets/resource/{}.json?api-version=v2".format(i["id"]))
+        except Exception as e:
+            logger.error(f"Failed to fetch password {i['id']}.")
+            logger.exception(e)
+            continue
+        try:
+            password = decode_json(passbolt_obj.decrypt(resource["body"]["data"]))
+        except ValueError as e:
+            logger.warning(f"Passbolt returned an empty secret for {i['id']}. This secret is not added to the passbolt pillar.")
+            continue
+        except Exception as e:
+            logger.error(f"Failed to decode password {i['id']}. It will not be added to the Pillar.")
+            continue
+        salt['passbolt'][i["id"]] = password
     return salt
 
 
-def decode_json(data):
+def decode_json(data: str) -> str:
     """
     The passbolt API returns legacy strings or JSON objects.
-    Try to decode JSON, and if invalid return string.
+    Try to decode JSON. Raise exceptions if the password is empty
+    or decoding fails.
+
+    For (obvious) security reasons empty passwords are never allowed.
     """
-    import json
     try:
         data = json.loads(data)
-    except json.decoder.JSONDecodeError:
-        return data
-    return data["password"]
+        password = data["password"]
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    if not password:
+        raise ValueError("Empty passwords are not allowed.")
+    return password
 
 
-def fetch_passbolt_passwords(group_uuid):
+def fetch_passbolt_passwords(group_uuid: str) -> dict:
     """
     Generate Passbolt API object and call API fetch function
     """
+    try:
+        uuid_obj = uuid.UUID(group_uuid, version=4)
+    except ValueError:
+        logger.error(f"{group_uuid} is not a valid UUID.")
+        return {}
+    logger.debug(f"Fetching passwords for group {group_uuid}")
     path = "/etc/salt/passbolt.ini"
     with passboltapi.PassboltAPI(config_path=path) as passbolt:
         salt = generate_pillar(passbolt, group_uuid)
